be1f7289253a380787dbb7f75d798be94eef70fc
filod lin <filod@qq.com>
2014-03-19 12:07:03 +0800

    updated dist at Wed Mar 19 2014 12:07:03 GMT+0800 (CST)

--- src/common/apiHandler.js
+++ src/common/apiHandler.js
@@ -70,6 +70,21 @@ ApiHandler = function (url) {
     return request;    
   } 
   
+  this.valueSent = function (params, load, error) {
+    
+    var request = apiRequest("valueSent");        
+    request.post(JSON.stringify(params))
+      .on('load', function(xhr) {
+        var response = JSON.parse(xhr.response);  
+        load(response);
+      })
+      .on('error', function(xhr){
+        console.log(xhr.response);
+        if (error) error({status:xhr.status,text:xhr.statusText,message:xhr.response})
+      });
+      
+    return request;      
+  }
   
   this.issuerCapitalization = function (params, load, error) {
     
@@ -128,7 +143,7 @@ ApiHandler = function (url) {
   
   this.getTopMarkets = function (callback, error) {
     var request = apiRequest("topMarkets");
-
+    
     request.post("")
     .on('load', function(xhr){   
       var response = JSON.parse(xhr.response);
@@ -141,7 +156,49 @@ ApiHandler = function (url) {
     });    
   }
   
+  this.getVolume24Hours = function (callback, error) {
+    var request = apiRequest("totalValueSent");
+
+    request.post(JSON.stringify({
+      exchange : {
+        currency : "USD",
+        issuer   : "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
+      }
+    }))
+    .on('load', function(xhr){   
+      var response = JSON.parse(xhr.response);
+        callback (response);
+      
+    }).on('error', function(xhr){
+      console.log(xhr.response);
+      if (error) error({status:xhr.status,text:xhr.statusText,message:xhr.response});
+    });     
+  }
   
+  this.getVolume30Days = function (callback, error) {
+    var request = apiRequest("totalValueSent");
+    
+    request.post(JSON.stringify({
+      endTime   : moment.utc(),
+      startTime : moment.utc().subtract(30, "days"),
+      exchange  : {
+        currency : "USD",
+        issuer   : "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
+      }
+    }))
+    .on('load', function(xhr){   
+      var response = JSON.parse(xhr.response);
+      console.log(response);
+        callback (response);
+      
+    }).on('error', function(xhr){
+      console.log(xhr.response);
+      if (error) error({status:xhr.status,text:xhr.statusText,message:xhr.response});
+    }); 
+    
+  }
+  
+/*  
   this.networkValue = function (params, callback, error) {
     var request = apiRequest("networkValue");
     request.post(JSON.stringify(params))
@@ -155,4 +212,5 @@ ApiHandler = function (url) {
     
     return request;    
   }
+*/  
 }
